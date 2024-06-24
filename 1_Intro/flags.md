64f9b981b9733ad505878464fcc5cdd0d6a154b5
pr1or1tyQ <user@example.com>
2024-06-24 03:52:14 +0000

    intro almost complete

---
+++ 1_Intro/flags.md
@@ -0,0 +1,11 @@
+These challenges are all rather basic so I am just including the flags here, they only require a minimal explanation and more details are included on the challenge website.
+
+
+pico ctf 2018 strings:
+- run the strings command
+- picoCTF{sTrIngS_sAVeS_Time_3f712a28}
+
+
+helithumper re
+- the code includes a simple loop that validates each character of input against the flag (which is included in the binary)
+- flag{hUCf_lAb} 
--- pwntools_template.py
+++ pwntools_template.py
@@ -1,1 +1,38 @@
-todo: this
+#!/usr/bin/env python3
+#todo: update this
+from pwn import *
+
+# connect to challeng problem at specified port
+chall_host = '192.168.2.99'
+chall_port = '55984'
+
+chall_filename = './temp_filename' # include path to access file
+context.bits = 32 # working with a 32 bit binary
+context.log_level = 'DEBUG'
+context(arch='i386', os='linux')
+
+if args.REMOTE: #if we want to run against the remote server
+    p = remote(chall_host, chall_port)
+elif args.GDB:
+    p = process(chall_filename) #program we are attacking
+    #gdb commands to run when attatching
+    gdbScript = '''
+    info func
+    '''
+    gdb.attach(p, aslr=False, gdbscript=gdbscript)
+else:
+    p = process(chall_filename)
+
+
+# Do the Stuff
+
+# Assemble Payload
+temp = b'temp'
+payload = b''.join(
+        [temp,p32(temp),p64(temp)
+    ]
+)
+p.sendline(payload)
+#p.recvall()
+p.interactive()
+p.close()
